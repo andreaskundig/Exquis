9ce43688a68645b248477e8ff941de788275bb0f
andreas <andreas.kuendig@gmx.ch>
2015-10-31 23:07:40 +0100

    vortex5, noisySlowCopy

---
+++ public/animations/noisySlowCopy.js
@@ -0,0 +1,24 @@
+define(["bibs/noise", "bibs/imageDataUtils"], function(noise, idu){
+return {draw: function (context, borders){
+            // paste current image one pixel down
+            //context.clearRect(0, 0, 150, 150);
+            // east
+            var scale = 50;
+
+            for (var x = 0; x < context.canvas.width / scale; x++) {
+                for (var y = 0; y < context.canvas.height / scale; y++) {
+                    // noise.simplex2 and noise.perlin2 return values between -1 and 1.
+                    var value = noise.simplex2(x / 10 + this.i, y / 10 + this.i);
+                    var rec = idu.rectangle(x * scale, y * scale, scale, scale) ;
+                    var horizontal = value < -0.5 || value > 0.5; 
+                    var direction = value < 0 ? -1 : 1;
+                    idu.pushLine(context, borders, rec, horizontal, 2*direction);
+                }
+            }
+
+            this.i += 0.002;
+        },
+        setup: function (context){
+            this.i = 0;
+        }};
+});
---
+++ public/animations/vortex5.js
@@ -0,0 +1,35 @@
+define(["bibs/canvasBuffer"], function(canvasBuffer){ return {
+    setup: function(context){
+        this.toRadians =  Math.PI / 180; 
+        
+        this.rotation = 0 ;
+        this.buffer = canvasBuffer.makeBuffer(context.canvas.width,
+                                              context.canvas.height);
+        this.delta = -1;                                      
+    },
+    
+    draw: function(context, borders){
+        
+        //copy borders on canvas
+        context.putImageData(borders.west, 0, 0);
+        context.putImageData(borders.east, context.canvas.width -1, 0);
+        context.putImageData(borders.north, 0, 0);
+        context.putImageData(borders.south, 0, context.canvas.height -1 );
+        
+        this.buffer.copyToBuffer(context, {x:0, y:0});
+        context.save();
+        context.translate(5, 85);
+        for(var i = 0; i<4 ; i++){
+
+            context.save();
+            context.rotate(this.rotation* this.toRadians + Math.PI*i/2);
+            this.buffer.copyFromBuffer(context);
+            context.restore();
+        }
+        context.restore();
+
+        this.rotation += this.delta;
+
+
+    }
+};});
