4ee8f4b7e554f45b0c377c87e743fc831bb448c3
andreas kundig <andreas.kuendig@gmx.ch>
2016-04-07 22:29:23 +0200

    remove snapshot, block categories

--- public/blk/image.js
+++ public/blk/image.js
@@ -1,31 +1,4 @@
 // https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#m929q2
-Blockly.Blocks['snapshot'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Snapshot");
-    this.appendValueInput("position")
-        .setCheck("point")
-        .appendField("origin");
-    this.appendValueInput("dimension")
-        .setCheck("dimension")
-        .appendField("dimension");
-    this.setOutput(true, "image");
-    this.setColour(165);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
-  }
-};
-
-Blockly.JavaScript['snapshot'] = function(block) {
-    var value_position = Blockly.JavaScript.valueToCode(block, 'position', Blockly.JavaScript.ORDER_ATOMIC);
-    var value_dimension = Blockly.JavaScript.valueToCode(block, 'dimension', Blockly.JavaScript.ORDER_ATOMIC);
-    var vdb = Blockly.JavaScript.variableDB_;
-    var rectVar = vdb.getDistinctName('rect', Blockly.Variables.NAME_TYPE);
-
-    var code = 'canvasBuffer('+value_dimension+')\n   .copyToBuffer(ctx, '+value_position+')';
-    return [code, Blockly.JavaScript.ORDER_ATOMIC];
-};
-
 Blockly.Blocks['blankImage'] = {
   init: function() {
     this.appendValueInput("dimension")
