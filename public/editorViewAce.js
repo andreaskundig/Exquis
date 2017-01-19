9615072921bcc562dc7b864d6546d9e67dfdf70f
david hodgetts <david.demainlalune@gmail.com>
2017-01-19 22:14:23 +0100

    fix editor visibility

--- public/editorViewAce.js
+++ public/editorViewAce.js
@@ -1,80 +1,8 @@
-define([], function(){
-    var makeButtonRow = function(controller){
-        var makeButtonRowDiv = function(){
-            //TODO make all ids unique by prefixing them with parent id,
-            // or even better, use classes instead, or firstchild etc..
-            var div = document.createElement('div');
-            div.innerHTML = [
-                '<div id="assemblage">',
-                '    <h2 class="assemblage_name"></h2> ',
-                '    <div id="assemblage-buttons">',
-                '        <button class="assemblage_load_button btn" type="button">load</button>',
-                '        <button class="assemblage_save_button btn" type="button">save</button>',
-                '        <button class="assemblage_save_as_button btn" type="button">save as</button>',
-                '    </div>',
-                '</div>',
-                '<div >',
-                '    <h3 class="filename_display"></h3> ',
-                '    <div id="editor-buttons">',
-                '        <button class="animation_save_button btn" type="button">save</button>',
-                '        <button class="animation_save_as_button btn" type="button">save as</button>',
-                '    </div>',
-                '</div>'].join('\n');
-            return div;
-        };
-        
-        var addAssemblageButtonListeners = function(displayAssemblageName, assController, editorParent){
-            var assemblageLoadButton = editorParent.querySelector(".assemblage_load_button"),
-                assemblageSaveButton = editorParent.querySelector(".assemblage_save_button"),
-                assemblageSaveAsButton = editorParent.querySelector(".assemblage_save_as_button");
-
-            var assemblageSaveAs = function(){
-                assController.saveAs().then(displayAssemblageName);
-            };
-
-            assemblageLoadButton.addEventListener('click', assController.load, true);
-            assemblageSaveButton.addEventListener('click', assController.save, true);
-            assemblageSaveAsButton.addEventListener('click', assemblageSaveAs, true);
-        };
-        
-        var addAnimationButtonListeners = function(displayAnimationName, animController, editorParent) {
-
-            var animSaveButton = editorParent.querySelector(".animation_save_button"),
-                animSaveAsButton = editorParent.querySelector(".animation_save_as_button");
-
-            
-            animSaveButton.addEventListener('click', animController.save, true);
-
-            var animSaveAs = function(){
-                animController.saveAs().then(displayAnimationName);
-            };
-            animSaveAsButton.addEventListener('click', animSaveAs, true);
-        };
-
-        var makeTextContentSetter = function(domElement){
-            return function(name){
-                domElement.textContent = name;
-            };
-        };
-
-        var buttonRowDiv = makeButtonRowDiv(),
-            displayAssemblageName = makeTextContentSetter(buttonRowDiv.querySelector(".assemblage_name")),
-            displayAnimationName = makeTextContentSetter(buttonRowDiv.querySelector(".filename_display"));
-        addAnimationButtonListeners(displayAnimationName,
-                                    controller.animController,
-                                    buttonRowDiv);
-        addAssemblageButtonListeners(displayAssemblageName,
-                                     controller.assController,
-                                     buttonRowDiv);
-        return {div: buttonRowDiv,
-                displayAssemblageName: displayAssemblageName,
-                displayAnimationName: displayAnimationName};
-    };
-
+define(['editorButtonRow'], function(makeButtonRow){
 
     var makeEditorDiv = function(buttonRow){
         var editorDiv = document.createElement('div');
-        editorDiv.className = 'invisible editor';
+        editorDiv.className = 'invisible';
         editorDiv.appendChild(buttonRow.div);
         editorDiv.insertAdjacentHTML('beforeend',
                                      '<div class="animation_editor"></div>');
@@ -155,7 +83,7 @@ define([], function(){
                 aceEditor.setValue(animSource.code);
                 aceEditor.getSession().selection.clearSelection();
                 
-                displayAnimationName(animationName);
+                buttonRow.displayAnimationName(animationName);
                 displayCodeValidity(true);
             };
             
--- public/editorViewBlockly.js
+++ public/editorViewBlockly.js
@@ -1,105 +1,45 @@
-define([], function(){
-    var makeEditorHtml = function(){
-        //TODO make all ids unique by prefixing them with parent id,
-        // or even better, use classes instead, or firstchild etc..
-        return ['<div class="invisible" id="editor">',
-                '    <div id="assemblage">',
-                '        <h2 id="assemblage_name"></h2> ',
-                '        <div id="assemblage-buttons">',
-                '            <button id="assemblage_load_button" class="btn" type="button">load</button>',
-                '            <button id="assemblage_save_button" class="btn" type="button">save</button>',
-                '            <button id="assemblage_save_as_button" class="btn" type="button">save as</button>',
-                '        </div>',
-                '    </div>',
-                '    <div >',
-                '        <h3 id="filename_display"></h3> ',
-                '        <div id="editor-buttons">',
-                '            <button id="animation_save_button" class="btn" type="button">save</button>',
-                '            <button id="animation_save_as_button" class="btn" type="button">save as</button>',
-                '        </div>',
-                '        <div id="animation_editor">',
-                '        </div>',
-                '    </div>',
-                '</div>'].join('\n');
+define(['editorButtonRow'], function(makeButtonRow){
+    
+    var makeEditorDiv = function(buttonRow){
+        var editorDiv = document.createElement('div');
+        editorDiv.className = 'invisible';
+        editorDiv.appendChild(buttonRow.div);
+        editorDiv.insertAdjacentHTML('beforeend',
+                                     '<div class="animation_editor"></div>');
+        return editorDiv;
     };
     
-    var makeEditorView = function(controller){
-        var assController = controller.assController,
-            animController = controller.animController,
-            textAreaController = controller.textAreaController;
+    var injectBlocklyIframe = function(id, editorParent){
+        return new Promise(function(resolve, reject){
+            var editorContainer = editorParent.querySelector(".animation_editor"),
+                iframe = document.createElement('iframe');
 
-        var makeTextContentSetter = function(domElement){
-            return function(name){
-                domElement.textContent = name;
-            };
-        };
-
-	var makeAssemblageButtons = function(displayAssemblageName){
- 	    var assemblageLoadButton = document.getElementById("assemblage_load_button"),
-	        assemblageSaveButton = document.getElementById("assemblage_save_button"),
-	        assemblageSaveAsButton = document.getElementById("assemblage_save_as_button");
-
-            var assemblageSaveAs = function(){
-                assController.saveAs().then(displayAssemblageName);
+            iframe.id = id;
+            iframe.src = '/blockly-editor.html';
+            iframe.onload = function() {
+                iframe.contentWindow.require( ["blockly-editor"], function(ed) {
+                    resolve(ed);
+                });
             };
 
-            assemblageLoadButton.addEventListener('click', assController.load, true);
-            assemblageSaveButton.addEventListener('click', assController.save, true);
-            assemblageSaveAsButton.addEventListener('click', assemblageSaveAs, true);
+            editorContainer.appendChild(iframe);
+        });
+    };
+    
+    var makeDisplayCodeValidity = function(){
+        return function(valid){
+            console.log('blockly code is valid:', valid);
         };
+    };
+    
+    var makeEditorView = function(controller, parentId){
         
-        var makeAnimationButtons = function(displayAnimationName) {
-
-	    var animSaveButton = document.getElementById("animation_save_button"),
-		animSaveAsButton = document.getElementById("animation_save_as_button");
-
-	    animSaveButton.addEventListener('click', animController.save, true);
-
-	    var animSaveAs = function(){
-                animController.saveAs().then(displayAnimationName);
-	    };
-	    animSaveAsButton.addEventListener('click', animSaveAs, true);
-	};
-
-        var makeDisplayCodeValidity = function(element){
-            return function(valid){
-                element.className = valid ? "code_valid" : "code_invalid";
-            };
-        };
-        
-	var editorDom = document.getElementById("editor"),
-            displayAssemblageName = makeTextContentSetter(document.getElementById("assemblage_name")),
-            displayAnimationName = makeTextContentSetter(document.getElementById("filename_display")),
-            animationEditor = document.getElementById("animation_editor"),
-            displayCodeValidity = makeDisplayCodeValidity(animationEditor); 
-        makeAnimationButtons(displayAnimationName);
-        makeAssemblageButtons(displayAssemblageName);
-        displayAssemblageName(assController.getAssemblageName());
-
-        var injectBlocklyIframe = function(id){
-            return new Promise(function(resolve, reject){
-                var editorContainer = document.getElementById("animation_editor"),
-                    iframe = document.createElement('iframe');
-
-                iframe.id = id;
-                iframe.src = '/blockly-editor.html';
-                iframe.onload = function() {
-                    iframe.contentWindow.require( ["blockly-editor"], function(ed) {
-                        resolve(ed);
-                    });
-                };
-
-                // TODO the ace editor sets listeners for the buttons.
-                // this is not the job of the editor, move it somewhere else...
-                // addAnimationButtonListeners(displayAnimationName);
-                // addAssemblageButtonListeners(displayAssemblageName);
-
-                // if(editorContainer.firstChild){
-                //     editorContainer.removeChild(editorContainer.firstChild);
-                // }
-                editorContainer.appendChild(iframe);
-            });
-        };
+        var editorParent = document.getElementById(parentId), 
+            buttonRow = makeButtonRow(controller),
+            editor = makeEditorDiv(buttonRow),
+            displayCodeValidity = makeDisplayCodeValidity() ; 
+        editorParent.appendChild(editor);
+        buttonRow.displayAssemblageName(controller.assController.getAssemblageName());
 
 	var setEditorContent = function(animationName, animSource){
             document.getElementById('blockly-editor')
@@ -107,24 +47,24 @@ define([], function(){
                 .require( ["blockly-editor"], function(ed) {
                     ed.setEditorContent(animSource.code);
                 });
-            displayAnimationName(animationName);
+            buttonRow.displayAnimationName(animationName);
             displayCodeValidity(true);
         };
 	
 	var theView = {
 	    setEditorContent: setEditorContent,
             show: function(){
-                editorDom.className = "editor_full_width";
+                editor.classList.remove("invisible");
             },
             hide: function(){
-                editorDom.className = "invisible";
+                editor.classList.add("invisible");
             },
             displayCodeValidity: displayCodeValidity
         };
         
-        return injectBlocklyIframe('blockly-editor').then(function(ed){
+        return injectBlocklyIframe('blockly-editor', editor).then(function(ed){
             ed.addChangeListener(function(animCode){
-                textAreaController.onCodeChange(animCode);
+                controller.textAreaController.onCodeChange(animCode);
             });
             return theView;
         });
