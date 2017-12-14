bc2a42284a18a9baa67b6729dc7e3a2c1ab9aa93
david hodgetts <david.demainlalune@gmail.com>
2017-12-14 22:17:29 +0100

    finishes animation vasaCol

--- public/animations/vasacol.js
+++ public/animations/vasacol.js
@@ -1,8 +1,8 @@
 define(["bibs/noise","paper","bibs/imageDataUtils", "bibs/shapes"], 
 function(noise, paper, idu, shapes){
     const squaresPerSide = 5,
-          squares = [];
-    let      color = null;
+          squares = [],
+          calculatedColors = [];
     const xy = index => [Math.floor(index / squaresPerSide),
                         index % squaresPerSide];
     const indexForXY = ([x,y]) => x * squaresPerSide + y;
@@ -11,25 +11,6 @@ function(noise, paper, idu, shapes){
         if(y == 0){ return 'north';}                        
         if(x == squaresPerSide - 1){ return 'east';}                        
         if(y == squaresPerSide - 1){ return 'south';}
-        return null;
-    };
-    const intersectsBorder = (x,y, square, canvasSize) => {
-        let border = neighboringBorder(x,y);
-        if(!border){
-            return null;
-        }
-        if(border == 'west' && square.bounds.left < 0){
-            return {border: border, size: square.bounds.left};
-        }                        
-        if(border == 'north' && square.bounds.top < 0){
-            return {border: border, size: square.bounds.top};
-        } 
-        if(border == 'east' && square.bounds.right > canvasSize){
-            return {border: border, size: canvasSize - square.bounds.right};
-        }
-        if(border == 'south' && square.bounds.bottom > canvasSize){
-            return {border: border, size: canvasSize - square.bounds.bottom};
-        }
         return null;
     };
     const colorIfIntersectsBorder = (x,y, square, borderColors, canvasSize) => {
@@ -65,10 +46,10 @@ function(noise, paper, idu, shapes){
             const p = new paper.PaperScope();
             p.setup(context.canvas);
             const stepSize = context.canvas.width / squaresPerSide,
-                  squareSize = 29,
+                  squareSize = 39,
                   centeringOffset = new p.Point(1,1)
                       .multiply((stepSize - squareSize)/2);
-            
+              
             for (var x = 0; x < squaresPerSide; x++) {
                 for (var y = 0; y < squaresPerSide; y++) {
                     const topLeft = new p.Point(x,y)
@@ -84,14 +65,20 @@ function(noise, paper, idu, shapes){
         },
         draw: function (context, borders){
 
-            let intersectionSizes = {east:0, west:0, north:0, south:0};
+            let colors = Object.keys(borders).reduce((acc, dir) => {
+                let avg = idu.averageColor(borders[dir]);
+                acc[dir] = `rgb(${avg.join(',')})`;
+                return acc;
+             } ,{});
+            let sizes = [],
+                coloredSquares = [];
             squares.forEach((square,index) =>{
                 const [x,y] = xy(index) ,
                       rotNoise = noise.simplex2(x / 10 + i, y / 10 + i),
                       sideNoise = noise.simplex2(x / 10 + j, y / 10 + j),
                       rotation = rotNoise * 180 * 0.3,
                       //                  scaling = .8,
-                      scaling = sideNoise* 0.49 + 1,
+                      scaling = sideNoise* 2 + 1,
                       relScaling = scaling / (square.oldScaling || 1),
                       relRotation = rotation - square.oldRotation || 0 ; 
                 //     side = scale * 0.9 * Math.abs(sideNoise * 0.6+0.9);
@@ -99,32 +86,41 @@ function(noise, paper, idu, shapes){
                 square.oldRotation = rotation;
                 square.scale(relScaling);
                 square.oldScaling = scaling;
-                let intersection = intersectsBorder(x,y,
-                                                    square,
-                                                    context.canvas.width);
-                if(intersection){
-                    intersectionSizes[intersection.border] += intersection.size;
-                }
-                square.fillColor = color;
+                sizes.push(scaling);
+                let borderColor = colorIfIntersectsBorder(x, y, 
+                                                      square, colors, 
+                                                      context.canvas.width),
+                    newColor = borderColor || calculatedColors[index];
+                square.fillColor = newColor;
+                coloredSquares.push([square, borderColor]);
             });
-            const biggestBorder = Object.keys(intersectionSizes)
-            .reduce((bb, border) =>{
-                const currentSize = intersectionSizes[border];
-                if(!bb.border || bb.size < currentSize){
-                    bb.border = border;
-                    bb.size = currentSize;
+            sizes.forEach((size,index) =>{
+                const [x,y] = xy(index);
+                if(!coloredSquares[index][1]){
+                    const sq = coloredSquares[index][0],
+                    neighbors = neighborsXYs(x,y)
+                    .map(indexForXY)
+                    .filter(i => sizes[i] > size)
+                    .sort((a,b) => sizes[b] - sizes[a])
+                    .map(i => squares[i]);
+                    //console.log(size);
+                    for(let i = 0; i < neighbors.length; i++){
+                        const neighbor = neighbors[i];
+                        if(sq.intersects(neighbor)){
+                            calculatedColors[index] = neighbor.fillColor;
+                            break;
+                        }
+                    }
+                    //console.log('no col')
                 }
-                return bb;
-            }, {});
-            if(biggestBorder.border){
-              let colorArray  = idu.averageColor(borders[biggestBorder.border]);
-              color = `rgb(${colorArray.join(',')})`
-            }else{
-                color = 'white';
-            }
-            
-            i += 0.003;
-            j += 0.004;
+                //return if square already has a color
+                // else take biggest intersecting neighbor's color
+                // keep same color
+                // put color in calculatedColor
+            });
+
+            i += 0.002;
+            j -= 0.003;
             
         }
     };
