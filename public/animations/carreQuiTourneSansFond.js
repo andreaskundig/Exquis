a240d9f3ad4a1896038f9921a5f4dd449de81800
david hodgetts <david.demainlalune@gmail.com>
2018-01-11 21:41:30 +0100

    adds makeLayer function in carreVoisin

---
+++ public/animations/carreQuiTourneSansFond.js
@@ -0,0 +1,18 @@
+define({
+        setup: function (context){
+            this.rotation = 0;
+        },
+        draw: function (context, borders){
+            context.fillStyle = "rgba(200,255,0,0)";
+
+            context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+            context.save();
+            context.translate(75, 75);
+            context.rotate(this.rotation);
+            context.fillStyle = "rgb(200,0,255)";
+            context.fillRect(-70, -90, 140, 100);
+            context.restore();
+            
+            this.rotation -= Math.PI / 180;
+        }});
--- public/animations/carreVoisin.js
+++ public/animations/carreVoisin.js
@@ -1,20 +1,33 @@
-define(["bibs/canvasBuffer", 
+define(["bibs/canvasBuffer",
         "/animations/carreQuiTourne.js", 
-        "/animations/vasa.js"], 
-function(canvasBuffer, anim1, anim2){
+        "/animations/carreQuiTourneSansFond.js",
+        "/animations/carreSinus.js"], 
+function(canvasBuffer, anim1, anim2, anim3){
+    var makeLayer = (anim, width, height) => {
+        var buffer = canvasBuffer(width, height);
+        anim.setup(buffer.context);
+        return {draw: borders => anim.draw(buffer.context, borders),
+                context: buffer.context,
+                pasteInto: buffer.pasteInto};
+    };
     return {
         setup: function (context){
             anim1.setup(context);
-            this.buffer = canvasBuffer(context.canvas.width,
-                                                  context.canvas.height);
-            anim2.setup(this.buffer.context);
+            const w = context.canvas.width;
+            const h = context.canvas.height;
+            
+            this.l2 = makeLayer(anim2, w, h);
+            this.l3 = makeLayer(anim3, w, h);
 
         },
         draw: function (context, borders){
             anim1.draw(context, borders);
-            anim2.draw(this.buffer.context, borders);
-
-            this.buffer.pasteInto(context,'destination-in');
+            this.l2.draw(borders);
+            this.l2.pasteInto(context,'destination-in');
+            
+            this.l3.draw(borders);
+            this.l2.pasteInto(this.l3.context,'destination-out');
+            this.l3.pasteInto(context);
         }
     };
 });
